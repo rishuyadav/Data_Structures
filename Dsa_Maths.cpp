37090dbce0be8d789b24f48f1ba6db4ac4103cbf
Rishu Yadav <rishuss1212@gmail.com>
2021-08-19 00:42:26 +0530

    Palindrome Testing

---
+++ Dsa_Maths.cpp
@@ -0,0 +1,22 @@
+//To check if a number is palindrome or not
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n, k, rev = 0;
+	cin >> n;
+	int temp = n;
+	while (temp > 0) {
+		k = temp % 10;
+		rev = rev * 10 + k;
+		temp = temp / 10;
+
+	}
+	if (rev == n) {
+		cout << "Yes";
+	}
+	else {
+		cout << "No";
+	}
+}
+
