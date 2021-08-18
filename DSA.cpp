93a8a3ef3d57b0f613e553fbd1803760fcb39621
Rishu Yadav <rishuss1212@gmail.com>
2021-08-18 17:42:33 +0530

    Deleting unnecessary files

--- DSA.cpp
+++ DSA.cpp
@@ -2,5 +2,20 @@
 using namespace std;
 int main()
 {
-	cout << "This marks the beginning of dsa";
+	int n, k, rev = 0;
+	cin >> n;
+	int temp = n;
+	while (temp > 0) {
+		k = temp % 10;
+		rev = rev * 10 + k;
+		temp = temp / 10;
+
+	}
+	if (rev == n) {
+		cout << "Yes";
+	}
+	else {
+		cout << "No";
+	}
 }
+
